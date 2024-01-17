c40024dda077273a7b9363688c848d9ea4c1d839
Aleksandar Panic <arekusanda1@gmail.com>
2024-01-17 10:05:03 +0100

    fixes

--- frontend/api.ts
+++ frontend/api.ts
@@ -1,12 +1,12 @@
 import { axiod, IAxiodResponse } from "./deps.ts";
 import { NoteRecord } from "$backend/repository/note-repository.ts";
-import { AddNoteRequest } from "../backend/api-handlers/notes/add-note.ts";
-import { FindGroupsRequest } from "../backend/api-handlers/groups/find-groups.ts";
-import { FindNotesRequest } from "../backend/api-handlers/notes/find-notes.ts";
+import { AddNoteRequest } from "$backend/api-handlers/notes/add-note.ts";
+import { FindGroupsRequest } from "$backend/api-handlers/groups/find-groups.ts";
+import { FindNotesRequest } from "$backend/api-handlers/notes/find-notes.ts";
 import { GroupRecord } from "$backend/repository/group-repository.ts";
 import { getUserData } from "$frontend/user-data.ts";
-import { AddGroupRequest } from "../backend/api-handlers/groups/add-group.ts";
-import { UpdateGroupRequest } from "../backend/api-handlers/groups/update-group.ts";
+import { AddGroupRequest } from "$backend/api-handlers/groups/add-group.ts";
+import { UpdateGroupRequest } from "$backend/api-handlers/groups/update-group.ts";
 
 const apiInterface = axiod.create({
   withCredentials: true,
