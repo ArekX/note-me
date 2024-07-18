4ef4dc71e9fa2b910e4452bae187e01f3d49058b
Aleksandar Panic <arekusanda1@gmail.com>
2024-07-18 12:22:35 +0200

    remove unused shell file

--- fedora-env.sh
+++
@@ -1,6 +0,0 @@
-echo Setting up Fedora env for NPM support
-
-# NPM on deno requires temp directory which is on same partition
-# otherwise cross-device link (error 18) occurs
-mkdir -p temp
-export TMPDIR=$(pwd)/temp
