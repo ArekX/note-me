2d4e511aa5846ce1e08a7773c2b60eb3fc854b75
Aleksandar <arekusanda1@gmail.com>
2024-07-20 15:31:43 +0200

    add unlock timeout

--- frontend/hooks/use-encryption-lock.ts
+++ frontend/hooks/use-encryption-lock.ts
@@ -1,4 +1,5 @@
 import { signal, useComputed } from "@preact/signals";
+import { addMessage } from "$frontend/toast-message.ts";
 
 export const UNLOCK_DURATION_MINUTES = 15;
 
@@ -8,24 +9,20 @@ interface PendingRequest {
 }
 
 const isLockWindowOpen = signal(false);
-const userPassword = signal<string | null>("");
-const unlockedUntil = signal<Date | null>(null);
+const userPassword = signal<string | null>(null);
 const pendingRequests = signal<PendingRequest[]>([]);
+const unlockTimeoutId = signal<number | null>(null);
 
 export const useEncryptionLock = () => {
-    const isLocked = useComputed(() => {
-        if (!unlockedUntil.value) {
-            return true;
-        }
-
-        return unlockedUntil.value < new Date();
-    });
-
     const unlock = (password: string) => {
         userPassword.value = password;
-        unlockedUntil.value = new Date(
-            Date.now() + 1000 * 60 * UNLOCK_DURATION_MINUTES,
+        clearTimeout(unlockTimeoutId.value!);
+
+        unlockTimeoutId.value = setTimeout(
+            lock,
+            1000 * 60 * UNLOCK_DURATION_MINUTES,
         );
+
         isLockWindowOpen.value = false;
 
         for (const request of pendingRequests.value) {
@@ -33,19 +30,30 @@ export const useEncryptionLock = () => {
         }
 
         pendingRequests.value = [];
+
+        addMessage({
+            type: "success",
+            text: "Protected notes are now unlocked.",
+        });
     };
 
     const lock = () => {
         isLockWindowOpen.value = false;
         userPassword.value = null;
-        unlockedUntil.value = null;
 
         for (const request of pendingRequests.value) {
             request.reject();
         }
 
         pendingRequests.value = [];
+
+        addMessage({
+            type: "info",
+            text: "Protected notes are now locked.",
+        });
     };
+
+    const isLocked = useComputed(() => userPassword.value === null);
 
     const requestPassword = () =>
         new Promise<string>((resolve, reject) => {
