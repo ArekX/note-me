eb7c3ce7f5930ec8225e8165154f9f731c8c7f3d
Aleksandar <arekusanda1@gmail.com>
2024-08-10 22:54:47 +0200

    fix check

--- check.ts
+++ check.ts
@@ -21,6 +21,7 @@ const variations: string[] = [
 
 const excludedStdErrStrings = [
     "\x1b[0m\x1b[32mCheck\x1b[0m",
+    "waiting for file lock on node_modules directory",
 ];
 
 const checkUrl = dirname(import.meta.url);
