44f098f7f5646ffa8fe358a336e1a00c0c45c9e6
Aleksandar Panic <arekusanda1@gmail.com>
2024-08-26 12:18:52 +0200

    remove tests

--- setup-githooks.sh
+++ setup-githooks.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 echo "deno task check" > .git/hooks/pre-commit
-echo "deno task test" > .git/hooks/pre-push
 chmod +x .git/hooks/pre-commit
-chmod +x .git/hooks/pre-push
 echo "Hooks installed."
--- tests/deps.ts
+++
@@ -1,1 +0,0 @@
-export { DOMParser } from "https://deno.land/x/deno_dom@v0.1.43/deno-dom-wasm.ts";
